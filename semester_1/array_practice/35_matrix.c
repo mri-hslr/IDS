0b5a9e459950409e107454bf2afb576d26880139
mri-hslr <mridulbhardwaj13@gmail.com>
2026-04-18 11:00:00 +0530

    Added 35

--- semester_1/array_practice/35_matrix.c
+++ semester_1/array_practice/35_matrix.c
@@ -8,5 +8,43 @@
 
 int main()
 {
-}
+    // Local decleratin
+    int a[20][20], n, m, i, j, num, multiplier = 1;
 
+    // Input Section
+    printf("Enter the order of matrix: ");
+    scanf("%d%d", &n, &m);
+    printf("Enter the value of number: ");
+    scanf("%d", &num);
+    for (i = 0; i < n; i++)
+    {
+        printf("Enter the elemts of row %d :", i + 1);
+        for (j = 0; j < m; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Loigc
+    for (i = 0; i < n; i++)
+    {
+        printf("Enter the elemts of row %d :", i + 1);
+        for (j = 0; j < m; j++)
+        {
+            if (a[i][j] == 0)
+            {
+                a[i][j] = num * (multiplier++);
+            }
+        }
+    }
+
+    // Output Section
+    for (i = 0; i < n; i++)
+    {
+        printf("\n");
+        for (j = 0; j < m; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+    }
+}
